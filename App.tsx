75f7f8387d3158f13fdf571aa2e95f2251e9f645
Max16 <max16@naver.com>
2022-02-14 09:44:37 +0900

    Day 22 Apploading

--- App.tsx
+++ App.tsx
@@ -1,7 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Ionicons } from "@expo/vector-icons";
+import { Asset } from "expo-asset";
+import * as Font from "expo-font";
+import { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import AppLoading from "expo-app-loading";
 
 export default function App() {
+  const [loading, setLoading] = useState(true);
+  const onFinish = () => {
+    setLoading(false);
+  };
+  const preload = async () => {
+    const fontsToLoad = [Ionicons.font];
+    const fontPromises = fontsToLoad.map((font) => Font.loadAsync(font));
+    const imagesToLoad = [
+      require("./assets/logo_black.png"),
+      require("./assets/logo_white.png"),
+    ];
+    const imagePromises = imagesToLoad.map((image) => Asset.loadAsync(image));
+    await Promise.all([...fontPromises, ...imagePromises]);
+  };
+  if (loading) {
+    return (
+      <AppLoading
+        startAsync={preload}
+        onError={console.warn}
+        onFinish={onFinish}
+      />
+    );
+  }
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
@@ -13,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ README.md
@@ -0,0 +1,7 @@
+## Instaclone Challenge App
+
+Making NomadCoffee App with React native, Expo, Apollo client, Graphql, Typescript
+
+### Day 22
+
+- [x] Apploading
